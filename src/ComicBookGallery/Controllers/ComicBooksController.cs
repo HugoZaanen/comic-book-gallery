56dd4f97426fb0ae04278025be3cf237e4e42f6e
Huego <hugozaanen@hotmail.com>
2019-02-24 13:08:20 +0100

    bla bla

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,7 +6,11 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBooksController : Controller
+    class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            return View();           
+        }
     }
 }
